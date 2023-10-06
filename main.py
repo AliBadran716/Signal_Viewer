d03d1dc009be5077595827d9131ac99ca7419199
AliBadran716 <102072821+AliBadran716@users.noreply.github.com>
2023-10-07 01:27:14 +0300

    linked radio buttons and push buttons

--- main.py
+++ main.py
@@ -15,12 +15,36 @@ class MainApp (QMainWindow,FORM_CLASS): # go to the main window in the form_clas
         super(MainApp,self).__init__(parent)
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.Handle_btn()
 
-    
+    def Handle_btn(self):
+        self.graph1_radio_btn.toggled.connect(self.graph1_selected)
+        self.graph2_radio_btn.toggled.connect(self.graph2_selected)
+        self.link_radio_btn.toggled.connect(self.link_selected)
+        self.speed_push_btn.clicked.connect(self.speed_changed)
+        self.play_push_btn.clicked.connect(self.play_changed)
+        self.rewind_push_btn.clicked.connect(self.rewind_changed)
+
+    def graph1_selected(self, enabled):
+        if enabled:
+            print('graph1')
+    def graph2_selected(self, enabled):
+        if enabled:
+            print('graph2')
+
+    def link_selected(self, enabled):
+        if enabled:
+            print('link')
+
+    def speed_changed(self):
+        print("speed change")
+
+    def play_changed(self,state):
+        print("play")
 
 
-
-
+    def rewind_changed(self):
+        print("rewind")
 
 
 
